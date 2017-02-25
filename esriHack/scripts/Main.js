10f405e81f652b467d394001c15bd55f0d5e3138
jan612 <jan612@cs.bham.ac.uk>
2017-02-25 20:14:00 +0000

    Started implementing drawCuboid in Main.js

--- esriHack/scripts/Main.js
+++ esriHack/scripts/Main.js
@@ -2,6 +2,7 @@ var gl; // A global variable for the WebGL context
 
 const BRICK_WIDTH = 2;
 const BRICK_HEIGHT = 2;
+const BRICK_DEPTH
 
 function start()
 {
@@ -42,9 +43,77 @@ function initWebGL(canvas)
     return gl;
 }
 
-function drawCuboid(x, y, h)
+function drawCuboid(x, z, h)
 {
-    
+    //VERTICIES
+
+    var x1 = x*BRICK_WIDTH;
+    var z1 = z*BRICK_DEPTH;
+
+    var vertices = [
+        //BOTTOM
+        x1, 0, z1,
+        x1+BRICK_WIDTH, 0, z1,
+        x1+BRICK_WIDTH, 0, z1+BRICK_DEPTH,
+        x1, 0, z1+BRICK_DEPTH,
+
+        //FRONT
+        x1, 0, z1,
+        x1+BRICK_WIDTH, 0, z1,
+        x1+BRICK_WIDTH, h, z1,
+        x1, h, z1,
+
+        //LEFT
+        x1+BRICK_WIDTH, 0, z1,
+        x1+BRICK_WIDTH, 0, z1+BRICK_DEPTH,
+        x1+BRICK_WIDTH, h, z1+BRICK_DEPTH,
+        x1+BRICK_WIDTH, h, z1,
+
+        //BACK
+        x1+BRICK_WIDTH, 0, z1+BRICK_DEPTH,
+        x1, 0, z1+BRICK_DEPTH,
+        x1, h, z1+BRICK_DEPTH,
+        x1+BRICK_WIDTH, h, z1+BRICK_DEPTH,
+
+        //RIGHT
+        x1, 0, z1,
+        x1, 0, z1+BRICK_DEPTH,
+        x1, h, z1+BRICK_DEPTH,
+        x1, h, z1,
+
+        //TOP
+        x1, h, z1,
+        x1+BRICK_WIDTH, h, z1,
+        x1+BRICK_WIDTH, h, z1+BRICK_DEPTH,
+        x1, h, z1+BRICK_DEPTH,
+    ];
+
+    //COLOURS
+
+    var colours = [];
+
+    for(var i = 0; i < 96; i++)
+    {
+        colours.append(1.0);
+    }
+
+    var cubeVerticesColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colours), gl.STATIC_DRAW);
+
+    //ELEMENTS
+
+    var cubeVerticesIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer)
+
+    var cubeVertexIndices = [
+        0,  1,  3,      1,  2,  3,    // bottom
+        5,  7,  6,      5,  4,  7,    // front
+        8,  9,  11,     8,  10, 9,   // left
+        12, 15, 14,     13, 12, 14,   // back
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23    // top
+    ];
 }
 
 function drawLegoMap(heightmap)
