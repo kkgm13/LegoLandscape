4f94bc4169e8f0213db05c9f500b98af8695e89f
jan612 <jan612@cs.bham.ac.uk>
2017-02-26 07:28:05 +0000

    Implemented gap filling for terrain

--- esriHack/scripts/Main.js
+++ esriHack/scripts/Main.js
@@ -50,31 +50,67 @@ var drawLegoLandscape = function (map, size) {
             var loader = new THREE.JSONLoader();
             loader.load('graphics/brick.json', function (geometry) {
 
-                var height = heightmap[jj*s + ii];
+                var height = heightmap[jj * s + ii];
 
-                var number =  height - Math.min();
+                var number = MAX_HEIGHT;
 
-                var material = new THREE.MeshLambertMaterial({
-                    color: parseInt(generateColour(height), 16),
-                    shading: THREE.FlatShading
-                });
+                if (jj > 0) {
+                    if (heightmap[(jj - 1) * s + ii] < number) {
+                        number = heightmap[(jj - 1) * s + ii];
+                    }
+                }
 
-                var cube = new THREE.Mesh(geometry, material);
+                if (jj < size - 1) {
+                    if (heightmap[(jj + 1) * s + ii] < number) {
+                        number = heightmap[(jj + 1) * s + ii];
+                    }
+                }
 
-                cube.position.x = ii * BRICK_WIDTH;
-                cube.position.y = Math.floor(height)*2.0;
-                cube.position.z = jj * BRICK_DEPTH;
-                cube.updateMatrix();
-                cube.matrixAutoUpdate = false;
+                if (ii > 0) {
+                    if (heightmap[jj * s + (ii - 1)] < number) {
+                        number = heightmap[jj * s + (ii - 1)];
+                    }
+                }
 
-                scene.add(cube);
+                if (ii < size - 1) {
+                    if (heightmap[jj * s + (ii + 1)] < number) {
+                        number = heightmap[jj * s + (ii + 1)];
+                    }
+                }
 
-                ii = ii+1;
+                //number = Math.floor(number) * BRICK_HEIGHT;
 
-                if(ii == s)
+                number = Math.ceil(height - number);
+
+                if(number < 1)
                 {
+                    number = 1;
+                }
+
+                for(var k = 0; k < number; k++) {
+                    var material = new THREE.MeshLambertMaterial({
+                        color: parseInt(generateColour(height - (k*BRICK_HEIGHT)), 16),
+                        shading: THREE.FlatShading
+                    });
+
+
+                    var cube = new THREE.Mesh(geometry, material);
+
+                    cube.position.x = ii * BRICK_WIDTH;
+                    cube.position.y = (Math.floor(height) * 2.0) - (k*BRICK_HEIGHT);
+                    cube.position.z = jj * BRICK_DEPTH;
+                    cube.updateMatrix();
+                    cube.matrixAutoUpdate = false;
+
+                    scene.add(cube);
+
+                }
+
+                ii = ii + 1;
+
+                if (ii == s) {
                     ii = 0;
-                    jj = jj+1;
+                    jj = jj + 1;
                 }
 
             });
@@ -82,56 +118,56 @@ var drawLegoLandscape = function (map, size) {
     }
 };
 
-var getHeightData = function(img) {
+var getHeightData = function (img) {
 
-    var canvas = document.createElement( 'canvas' );
+    var canvas = document.createElement('canvas');
     var sq = img.width
     canvas.width = sq;
     canvas.height = sq;
-    var context = canvas.getContext( '2d' );
+    var context = canvas.getContext('2d');
 
     var size = sq * sq, data = [];
 
-    context.drawImage(img,0,0);
+    context.drawImage(img, 0, 0);
 
-    for ( var i = 0; i < size; i ++ ) {
+    for (var i = 0; i < size; i++) {
         data[i] = 0
     }
 
     var imgd = context.getImageData(0, 0, sq, sq);
     var pix = imgd.data;
 
-    var j=0;
+    var j = 0;
     for (var i = 0; i < pix.length; i += (4)) {
-        var all = pix[i]+pix[i+1]+pix[i+2];
-        data[j++] = all/30;
+        var all = pix[i] + pix[i + 1] + pix[i + 2];
+        data[j++] = all / 30;
     }
 
     return data;
 }
 
-var getColours = function(img) {
+var getColours = function (img) {
 
-    var canvas = document.createElement( 'canvas' );
+    var canvas = document.createElement('canvas');
     var sq = img.width
     canvas.width = sq;
     canvas.height = sq;
-    var context = canvas.getContext( '2d' );
+    var context = canvas.getContext('2d');
 
     var size = sq * sq, data = [];
 
-    context.drawImage(img,0,0);
+    context.drawImage(img, 0, 0);
 
-    for ( var i = 0; i < size; i ++ ) {
+    for (var i = 0; i < size; i++) {
         data[i] = 0
     }
 
     var imgd = context.getImageData(0, 0, sq, sq);
     var pix = imgd.data;
 
-    var j=0;
+    var j = 0;
     for (var i = 0; i < pix.length; i += (4)) {
-        var all = [pix[i],pix[i+1],pix[i+2]];
+        var all = [pix[i], pix[i + 1], pix[i + 2]];
         data[j++] = all;
     }
 
@@ -146,15 +182,15 @@ loader.load(
     // resource URL
     imgPath,
     // Function when resource is loaded
-    function ( image ) {
+    function (image) {
         drawLegoLandscape(getHeightData(image), image.width);
     },
     // Function called when download progresses
-    function ( xhr ) {
+    function (xhr) {
 
     },
     // Function called when download errors
-    function ( xhr ) {
+    function (xhr) {
 
     }
 );
