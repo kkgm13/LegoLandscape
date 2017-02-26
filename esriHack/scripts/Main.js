4569a07e687538da1d625fa966293f6a47c397d9
jan612 <jan612@cs.bham.ac.uk>
2017-02-26 02:20:00 +0000

    Added heightmap visualisation, model loading in progress

--- esriHack/scripts/Main.js
+++ esriHack/scripts/Main.js
@@ -1,123 +1,81 @@
-var gl; // A global variable for the WebGL context
+var WIDTH = 699;
+var HEIGHT = 500;
 
-const BRICK_WIDTH = 2;
+const BRICK_WIDTH = 4;
 const BRICK_HEIGHT = 2;
-const BRICK_DEPTH
+const BRICK_DEPTH = 4;
 
-function start()
-{
-    var canvas = document.getElementById('glCanvas');
+const MAX_HEIGHT = 30; //600 will be good
 
-    // Initialize the GL context
-    gl = initWebGL(canvas);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
 
-    // Only continue if WebGL is available and working
-    if (!gl)
-    {
-        return;
+light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+light = new THREE.DirectionalLight(0x002288);
+light.position.set(-1, -1, -1);
+scene.add(light);
+
+light = new THREE.AmbientLight(0x222222);
+scene.add(light);
+
+var controls = new THREE.OrbitControls(camera);
+controls.addEventListener('change', render);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(WIDTH, HEIGHT);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+var generateColour = function (height) {
+    var colourInt = (268435455 * height) / MAX_HEIGHT;
+    return colourInt.toString(16);
+}
+
+var drawLegoLandscape = function (heightmap, size) {
+
+    for (var j = 0; j < size; j++) {
+        for (var i = 0; i < size; i++) {
+            //var geometry = new THREE.BoxGeometry( BRICK_WIDTH, heightmap[i][j], BRICK_DEPTH);
+
+            var cube = null;
+
+            var loader = new THREE.JSONLoader();
+            loader.load('./graphics/brick.json', function (geometry) {
+                var material = new THREE.MeshLambertMaterial({
+                    color: generateColour(heightmap[i][j]),
+                    shading: THREE.FlatShading
+                });
+                cube = new THREE.Mesh(geometry, material);
+
+                cube.position.x = i * BRICK_WIDTH;
+                cube.position.z = j * BRICK_DEPTH;
+                cube.position.y = (heightmap[i][j] / 2.0);
+                cube.updateMatrix();
+                cube.matrixAutoUpdate = false;
+
+                scene.add(cube);
+            });
+            
+        }
     }
+};
 
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Enable depth testing
-    gl.enable(gl.DEPTH_TEST);
-    // Near things obscure far things
-    gl.depthFunc(gl.LEQUAL);
-    // Clear the color as well as the depth buffer.
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-}
+var map = [
+    [4.0, 5.0, 6.0],
+    [3.0, 4.0, 5.0],
+    [2.0, 3.0, 4.0]
+];
 
-function initWebGL(canvas)
-{
-    gl = null;
+drawLegoLandscape(map, 3);
 
-    // Try to grab the standard context. If it fails, fallback to experimental.
-    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+var render = function () {
+    requestAnimationFrame(render);
 
-    // If we don't have a GL context, give up now
-    if (!gl)
-    {
-        alert('Unable to initialize WebGL. Your browser may not support it.');
-    }
+    renderer.render(scene, camera);
+};
 
-    return gl;
-}
-
-function drawCuboid(x, z, h)
-{
-    //VERTICIES
-
-    var x1 = x*BRICK_WIDTH;
-    var z1 = z*BRICK_DEPTH;
-
-    var vertices = [
-        //BOTTOM
-        x1, 0, z1,
-        x1+BRICK_WIDTH, 0, z1,
-        x1+BRICK_WIDTH, 0, z1+BRICK_DEPTH,
-        x1, 0, z1+BRICK_DEPTH,
-
-        //FRONT
-        x1, 0, z1,
-        x1+BRICK_WIDTH, 0, z1,
-        x1+BRICK_WIDTH, h, z1,
-        x1, h, z1,
-
-        //LEFT
-        x1+BRICK_WIDTH, 0, z1,
-        x1+BRICK_WIDTH, 0, z1+BRICK_DEPTH,
-        x1+BRICK_WIDTH, h, z1+BRICK_DEPTH,
-        x1+BRICK_WIDTH, h, z1,
-
-        //BACK
-        x1+BRICK_WIDTH, 0, z1+BRICK_DEPTH,
-        x1, 0, z1+BRICK_DEPTH,
-        x1, h, z1+BRICK_DEPTH,
-        x1+BRICK_WIDTH, h, z1+BRICK_DEPTH,
-
-        //RIGHT
-        x1, 0, z1,
-        x1, 0, z1+BRICK_DEPTH,
-        x1, h, z1+BRICK_DEPTH,
-        x1, h, z1,
-
-        //TOP
-        x1, h, z1,
-        x1+BRICK_WIDTH, h, z1,
-        x1+BRICK_WIDTH, h, z1+BRICK_DEPTH,
-        x1, h, z1+BRICK_DEPTH,
-    ];
-
-    //COLOURS
-
-    var colours = [];
-
-    for(var i = 0; i < 96; i++)
-    {
-        colours.append(1.0);
-    }
-
-    var cubeVerticesColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colours), gl.STATIC_DRAW);
-
-    //ELEMENTS
-
-    var cubeVerticesIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer)
-
-    var cubeVertexIndices = [
-        0,  1,  3,      1,  2,  3,    // bottom
-        5,  7,  6,      5,  4,  7,    // front
-        8,  9,  11,     8,  10, 9,   // left
-        12, 15, 14,     13, 12, 14,   // back
-        16, 17, 18,     16, 18, 19,   // right
-        20, 21, 22,     20, 22, 23    // top
-    ];
-}
-
-function drawLegoMap(heightmap)
-{
-
-}
-
+render();
